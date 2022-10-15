ffc65efc98dfdfa420e57705fc17c0262595728b
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-15 13:01:07 +1030

    Added event listeners for each appened button, on click prints to customlistEl

--- assets/scripts/stash.js
+++ assets/scripts/stash.js
@@ -1,16 +1,30 @@
 //query selectors
 var prePopEl = document.querySelector('#pre-pop');
+var customListEl = document.querySelector('#custom-list');
 
 // consts and hoisted vars
 var basicItems = ['water', 'poncho', 'boots', 'snacks', 'maps'];
+var customItems = [];
 
 //When I open stash page i am presented with a a list of default items to take on my trip
-function PopulteBasic() {
-    for (var i = 0; i < basicItems.length; i++) {
+function populteBasic() {
+    basicItems.forEach(i => {
         var buttonBasic = document.createElement('button');
-        buttonBasic.innerHTML = basicItems[i];
+        buttonBasic.setAttribute('id', i);
+        buttonBasic.innerHTML = i;
+        buttonBasic.addEventListener('click', function() {
+            var listCustom = document.createElement('p');
+            listCustom.innerHTML = i;
+            customListEl.appendChild(listCustom);
+        });
         prePopEl.appendChild(buttonBasic);
-    };
+    });
 };
 
-PopulteBasic();
+//When I click an item from the the prepopulted list it is added to users custom list and removed from basic items
+function populteCustom() {
+    console.log('populate custom');
+};
+
+
+populteBasic();
