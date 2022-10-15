7bcd1f2cc93c8f05aefb97c4d81840c67621f092
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-15 13:18:12 +1030

    onclick buttons will now disapear from prepop list and only display in custom list

--- assets/scripts/stash.js
+++ assets/scripts/stash.js
@@ -8,15 +8,21 @@ var customItems = [];
 
 //When I open stash page i am presented with a a list of default items to take on my trip
 function populteBasic() {
+// creates buttons from basic items array
     basicItems.forEach(i => {
         var buttonBasic = document.createElement('button');
-        buttonBasic.setAttribute('id', i);
+        buttonBasic.setAttribute('id', i); //set ID for for each created button to value of button
         buttonBasic.innerHTML = i;
-        buttonBasic.addEventListener('click', function() {
+
+        buttonBasic.addEventListener('click', function() { //event listener for each created button
+            var buttRemove = document.getElementById(i);
+            buttRemove.parentNode.removeChild(buttRemove);
             var listCustom = document.createElement('p');
-            listCustom.innerHTML = i;
+            listCustom.innerHTML = i; //content for appened from forEach
+            customItems.push(i); //adds clicked item to custom items array
             customListEl.appendChild(listCustom);
         });
+
         prePopEl.appendChild(buttonBasic);
     });
 };
