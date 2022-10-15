840e9c02fa9c6444eae2742c9f099cac7188dcfc
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-15 16:22:35 +1030

    saves and retrieves from localstorage, also have if stateemnt that does not display default items if custom list is populated

--- assets/scripts/stash.js
+++ assets/scripts/stash.js
@@ -15,54 +15,64 @@ var userTrip = {
 
 //When I open stash page i am presented with a a list of default items to take on my trip
 function populteBasic() {
-// creates buttons from basic items array
-    basicItems.forEach(i => {
-        var buttonBasic = document.createElement('button');
-        buttonBasic.setAttribute('id', i); //set ID for for each created button to value of button
-        buttonBasic.innerHTML = i;
+    if (customItems = null) {
+        basicItems.forEach(i => { // creates buttons from basic items array
+            var buttonBasic = document.createElement('button');
+            buttonBasic.setAttribute('id', i); //set ID for for each created button to value of button
+            buttonBasic.innerHTML = i;
 
-        buttonBasic.addEventListener('click', function() { //event listener for each created button
-            var buttRemove = document.getElementById(i);
-            buttRemove.parentNode.removeChild(buttRemove); //removes button after clicking
+            buttonBasic.addEventListener('click', function () { //event listener for each created button
+                var buttRemove = document.getElementById(i);
+                buttRemove.parentNode.removeChild(buttRemove); //removes button after clicking
 
-            var listCustom = document.createElement('p');
-            listCustom.innerHTML = i;
-            customItems.push(i);
-            customListEl.appendChild(listCustom);
+                var listCustom = document.createElement('p');
+                listCustom.innerHTML = i;
+                customItems.push(i);
+                customListEl.appendChild(listCustom);
 
+                localStorage.setItem('customItems', JSON.stringify(customItems));
+                // //FOR CHECKBOX'S
+                // var listCustom = document.createElement('input');
+                // listCustom.type = 'checkbox';
+                // listCustom.name = 'name';
+                // listCustom.value = 'value';
+                // listCustom.className = 'customBox'
+                // listCustom.id = i;
 
+                // var label = document.createElement('label');
+                // label.htmlFor = i;
+                // label.appendChild(document.createTextNode(i))
 
-            // //FOR CHECKBOX'S
-            // var listCustom = document.createElement('input');
-            // listCustom.type = 'checkbox';
-            // listCustom.name = 'name';
-            // listCustom.value = 'value';
-            // listCustom.className = 'customBox'
-            // listCustom.id = i;
+                // customIdEl.appendChild(listCustom);
+                // customListEl.appendChild(label);
+                // customItems.push(i); //adds clicked item to custom items array
+                // console.log(userTrip);
+            });
 
-            // var label = document.createElement('label');
-            // label.htmlFor = i;
-            // label.appendChild(document.createTextNode(i))
-
-            // customIdEl.appendChild(listCustom);
-            // customListEl.appendChild(label);
-            // customItems.push(i); //adds clicked item to custom items array
-            // console.log(userTrip);
-            
-
+            prePopEl.appendChild(buttonBasic);
         });
-
-        prePopEl.appendChild(buttonBasic);
-    });
+    } else {
+        populteCustom();
+    }
 };
 
 //When I click an item from the the prepopulted list it is added to users custom list and removed from basic items
 function populteCustom() {
-    console.log('populate custom');
+    var customItems = JSON.parse(localStorage.getItem('customItems'));
+    if (customItems != null) {
+        for (var i = 0; i < customItems.length; i++) {
+            var listCreate = document.createElement('p');
+            listCreate.innerHTML = customItems[i];
+            customListEl.appendChild(listCreate);
+        }
+    } else {
+        return;
+    }
 };
 
 
 populteBasic();
+//populteCustom();
 
 
 
