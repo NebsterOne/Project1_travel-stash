aedaed1ea59117f3e11915ce4052318fac0f175d
djmccartney <jamdancle@gmail.com>
2022-10-15 15:59:04 +1030

    last commit before I delete ans start again

--- assets/weather.js
+++ assets/weather.js
@@ -6,20 +6,19 @@ var desc = document.querySelector('.desc')
 var temp = document.querySelector('.temp')
 
 
-button.addEventListener('click',function() {
-    fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&appid=549aae77a3dc08100f08e1988c47e726&units=metric')
+button.addEventListener('click', function () {
+     fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + inputValue.value + '&appid=549aae77a3dc08100f08e1988c47e726&units=metric')
+
         .then(response => response.json())
-        .then(data => {
-            var name = data['name'];
-            var temp = data['main']['temp'];
-            var desc = data['weather'][0]['description'];
+        .then(data => console.log(data))
+
+            var nameValue = data['name'];
+        //    var tempValue = data['main']['temp'];
+        //    var descValue = data['weather'][0]['description'];
 
             name.innerHTML = nameValue
-            temp.innerHTML = tempValue
-            desc.innerHTML = descValue
-        })
-.catch(err => alert('Wrong city name'))
+        //    temp.innerHTML = tempValue
+        //    desc.innerHTML = descValue
+        .catch(err => alert('Wrong city name'))
 })
 
-
-// api.openweathermap.org/data/2.5/forecast?q=adelaide&units=metric&appid=549aae77a3dc08100f08e1988c47e726
