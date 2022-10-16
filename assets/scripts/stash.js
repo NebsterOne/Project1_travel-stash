fd1b1bb16bd348cdb8c6e1335a2c643970ab14f5
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-16 15:03:24 +1030

    Started to add the abilty to check off items already packed

--- assets/scripts/stash.js
+++ assets/scripts/stash.js
@@ -1,59 +1,59 @@
 //query selectors
 var prePopEl = document.querySelector('#pre-pop');
 var customListEl = document.querySelector('#custom-list');
-var customIdEl = document.querySelector('#custom-id');
+var packedListEl = document.querySelector('#packed-list');
 var addMoreBtn = document.querySelector('#add-more');
 
 // consts and hoisted vars
 var basicItems = ['water', 'poncho', 'boots', 'snacks', 'maps'];
 
-var customItems = [];
-var customStatus = [];
-var newItemArr = []
+var customItems = []; //array of items inside #custom list
+var customPacked = []; //array of items inside #packed list
+var newItemArr = [];
+//object to hold all data, NOT IN USE
 var userTrip = {
     items: customItems,
-    itemsChecked: customStatus
+    itemsPacked: customPacked
 };
 
 //When I open stash page i am presented with a a list of default items to take on my trip
 function populteBasic() {
 
-        basicItems.forEach(i => { // creates buttons from basic items array
-            var buttonBasic = document.createElement('button');
-            buttonBasic.setAttribute('id', i); //set ID for for each created button to value of button
-            buttonBasic.innerHTML = i;
+    basicItems.forEach(i => { // creates buttons from basic items array
+        var buttonBasic = document.createElement('button');
+        buttonBasic.setAttribute('id', i); //set ID for for each created button to value of button
+        buttonBasic.innerHTML = i;
 
-            buttonBasic.addEventListener('click', function () { //event listener for each created button
-                var buttRemove = document.getElementById(i);
-                buttRemove.parentNode.removeChild(buttRemove); //removes button after clicking
+        buttonBasic.addEventListener('click', function () { //event listener for each created button
+            var buttRemove = document.getElementById(i);
+            buttRemove.parentNode.removeChild(buttRemove); //removes button after clicking
 
-                var listCustom = document.createElement('p');
-                listCustom.innerHTML = i;
-                customItems.push(i);
-                customListEl.appendChild(listCustom);
+            var listCustom = document.createElement('button');
+            listCustom.innerHTML = i;
+            listCustom.setAttribute('id', i);
+            customItems.push(i);
+            customListEl.appendChild(listCustom);
 
-                localStorage.setItem('customItems', JSON.stringify(customItems));
-                // //FOR CHECKBOX'S
-                // var listCustom = document.createElement('input');
-                // listCustom.type = 'checkbox';
-                // listCustom.name = 'name';
-                // listCustom.value = 'value';
-                // listCustom.className = 'customBox'
-                // listCustom.id = i;
+            localStorage.setItem('customItems', JSON.stringify(customItems));
+            // //FOR CHECKBOX'S
+            // var listCustom = document.createElement('input');
+            // listCustom.type = 'checkbox';
+            // listCustom.name = 'name';
+            // listCustom.value = 'value';
+            // listCustom.className = 'customBox'
+            // listCustom.id = i;
 
-                // var label = document.createElement('label');
-                // label.htmlFor = i;
-                // label.appendChild(document.createTextNode(i))
+            // var label = document.createElement('label');
+            // label.htmlFor = i;
+            // label.appendChild(document.createTextNode(i))
 
-                // customIdEl.appendChild(listCustom);
-                // customListEl.appendChild(label);
-                // customItems.push(i); //adds clicked item to custom items array
-                // console.log(userTrip);
-            });
-
-            prePopEl.appendChild(buttonBasic);
+            // customIdEl.appendChild(listCustom);
+            // customListEl.appendChild(label);
+            // customItems.push(i); //adds clicked item to custom items array
+            // console.log(userTrip);
         });
-
+        prePopEl.appendChild(buttonBasic);
+    });
 };
 
 //When I click an item from the the prepopulted list it is added to users custom list and removed from basic items
@@ -61,8 +61,10 @@ function populteCustom() {
     var customItems = JSON.parse(localStorage.getItem('customItems'));
     if (customItems != null) {
         for (var i = 0; i < customItems.length; i++) {
-            var listCreate = document.createElement('p');
+            var listCreate = document.createElement('button');
+            listCreate.setAttribute('id', customItems[i]);
             listCreate.innerHTML = customItems[i];
+            listCreate.addEventListener('click', packedList)
             customListEl.appendChild(listCreate);
         }
     } else {
@@ -75,16 +77,42 @@ function addMore() {
     var newItem = document.querySelector('#add-more-cont').value;
     newItemArr = newItem;
     customItems.push(newItemArr);
-    var listCreate = document.createElement('p');
-        listCreate.innerHTML = newItem;
-        customListEl.appendChild(listCreate);
-        localStorage.setItem('customItems', JSON.stringify(customItems));
+    var listCreate = document.createElement('button');
+    listCreate.innerHTML = newItem;
+    listCreate.setAttribute('id', newItem);
+    listCreate.addEventListener('click', packedList);
+    customListEl.appendChild(listCreate);
+    localStorage.setItem('customItems', JSON.stringify(customItems));
+
+};
+
+function packedList(evt) {
+    var customItems = JSON.parse(localStorage.getItem('customItems'));
+    var clickedItem = (evt.target.id); //gets id of clicked button
+
     
+    console.log('clicked item ' + clickedItem);
+    console.log('custom item array from local storage ' + customItems);
+
+    var clickedItemAdd = document.getElementById(clickedItem); //query selector for clicked button
+    clickedItemAdd.parentNode.removeChild(clickedItemAdd); //removes button
+    var moveToPacked = document.createElement('button');
+    moveToPacked.innerHTML  = clickedItem;
+    console.log(typeof clickedItem);
+    moveToPacked.setAttribute('id', moveToPacked);
+    moveToPacked.addEventListener('click', unpackList);
+    packedListEl.appendChild(moveToPacked);
+
+};
+
+function unpackList() {
+console.log('unpack');
 };
 
 addMoreBtn.addEventListener('click', addMore);
 populteBasic();
 populteCustom();
+
 
 
 
