994b0f1e970befba6866460b2b2e84cce6e6c765
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-17 20:38:34 +1030

    gathering location and date data

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,8 +1,32 @@
 var newTripEL = document.querySelector('#new-trip');
 var createNewBtn = document.querySelector('#new-btn');
+var tripSubmit = document.querySelector('#user-sub');
 
-function newTrip() {
+
+function tripShowHide() {
 console.log('newTrip function start');
+if (newTripEL.style.display === 'none') {
+    newTripEL.style.display = 'block';
+} else {
+    newTripEL.style.display = 'none';
+}
 };
 
-createNewBtn.addEventListener('click', newTrip);
+function createNewTrip() {
+    var userLocation = document.querySelector('#u-location').value;
+    var userDate = document.querySelector('#u-date').value;
+
+        localStorage.setItem('userLocation', JSON.stringify(userLocation));
+        localStorage.setItem('userdate', JSON.stringify(userDate));
+        console.log(userLocation);
+        console.log(userDate);
+        
+
+};
+
+createNewBtn.addEventListener('click', tripShowHide);
+tripSubmit.addEventListener('click', createNewTrip);
+
+$(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
