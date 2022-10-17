01f8a52880056bd03d77e5ff43caa767f3146bcb
Nicolas Fraenkel <nicfraenkel@gmail.com>
2022-10-17 20:10:33 +1030

    quick update to css to hide new trip div by default

---
+++ assets/scripts/dashboard.js
@@ -0,0 +1,8 @@
+var newTripEL = document.querySelector('#new-trip');
+var createNewBtn = document.querySelector('#new-btn');
+
+function newTrip() {
+console.log('newTrip function start');
+};
+
+createNewBtn.addEventListener('click', newTrip);
